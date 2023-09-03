edf179426e11295b3ab1a26adc95dc116c4fa96e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2023-09-04 00:31:17 +0200

    Refractored pytest helper file

--- tests/helper.py
+++ tests/helper.py
@@ -4,20 +4,19 @@ Contains helper functions for the test routines.
 
 import subprocess
 import os
+from typing import Callable, Any
+
+webstorage_location: str = "http://<remote:database>/"
+test_data_location: str = "test_data/"
 
 
-webstorage_location = "http://virgodb.cosma.dur.ac.uk/swift-webstorage/IOExamples/"
-test_data_location = "test_data/"
-
-
-def requires(filename):
+def requires(filename: str) -> Callable:
     """
     Use this as a decorator around tests that require data.
     """
 
     # First check if the test data directory exists
     if not os.path.exists(test_data_location):
-
         os.mkdir(test_data_location)
 
     file_location = f"{test_data_location}{filename}"
@@ -36,10 +35,9 @@ def requires(filename):
         # It wrote an empty file, kill it.
         subprocess.call(["rm", file_location])
 
-        def dont_call_test(func):
+        def dont_call_test(func: Callable) -> Callable:
 
-            def empty(*args, **kwargs):
-
+            def empty(*args, **kwargs) -> bool:
                 return True
 
             return empty
@@ -48,15 +46,12 @@ def requires(filename):
 
     else:
 
-        def do_call_test(func):
+        def do_call_test(func: Callable) -> Callable:
 
-            def final_test():
-
+            def final_test() -> Any:
                 # Return the path on there for good measure
                 return func(f"{test_data_location}{filename}")
 
             return final_test
 
         return do_call_test
-
-    raise Exception("You should have never gotten here.")
